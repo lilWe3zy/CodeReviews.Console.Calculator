c974f58843f4c816669aa14a430d633480e0a774
armin <arminnel@proton.me>
2024-04-14 13:22:13 +0200

    add final refactor pass before features

--- lilWe3zy.Calculator/CalculatorLibrary/CalculatorLibrary.cs
+++ lilWe3zy.Calculator/CalculatorLibrary/CalculatorLibrary.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 
 namespace CalculatorLibrary;
 
@@ -9,8 +8,9 @@ public class Calculator
 
     public Calculator()
     {
-        StreamWriter logFile = File.CreateText("calculatorlog.json");
+        StreamWriter logFile = File.CreateText("calc-log.json");
         logFile.AutoFlush = true;
+
         _writer = new JsonTextWriter(logFile);
         _writer.Formatting = Formatting.Indented;
         _writer.WriteStartObject();
@@ -18,39 +18,42 @@ public class Calculator
         _writer.WriteStartArray();
     }
 
-    public double DoOperation(double num1, double num2, string op)
+    public double DoOperation(double firstOperand, double secondOperand, string operation)
     {
-        double
-            result = double
-                .NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
+        double result = double.NaN;
+
         _writer.WriteStartObject();
         _writer.WritePropertyName("Operand1");
-        _writer.WriteValue(num1);
+        _writer.WriteValue(firstOperand);
         _writer.WritePropertyName("Operand2");
-        _writer.WriteValue(num2);
+        _writer.WriteValue(secondOperand);
         _writer.WritePropertyName("Operation");
-        // Use a switch statement to do the math.
-        switch (op)
+
+        switch (operation)
         {
             case "a":
-                result = num1 + num2;
+                result = firstOperand + secondOperand;
                 _writer.WriteValue("Add");
                 break;
             case "s":
-                result = num1 - num2;
+                result = firstOperand - secondOperand;
                 _writer.WriteValue("Subtract");
                 break;
             case "m":
-                result = num1 * num2;
+                result = firstOperand * secondOperand;
                 _writer.WriteValue("Multiply");
                 break;
             case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0) result = num1 / num2;
+                // Catch zero division
+                if (secondOperand != 0)
+                {
+                    result = firstOperand / secondOperand;
+                }
+
                 _writer.WriteValue("Divide");
                 break;
-            // Return text for an incorrect option entry.
             default:
+                Console.WriteLine("I could not understand that option, please try again.");
                 break;
         }
 
--- lilWe3zy.Calculator/lilWe3zy.Calculator/Program.cs
+++ lilWe3zy.Calculator/lilWe3zy.Calculator/Program.cs
@@ -2,47 +2,38 @@
 
 namespace lilWe3zy.Calculator;
 
-internal static class Program
+internal static partial class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
+        var calculator = new CalculatorLibrary.Calculator();
         bool endApp = false;
-        // Display title as the C# console calculator app.
+
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
 
-        var calculator = new CalculatorLibrary.Calculator();
         while (!endApp)
         {
-            // Declare variables and set to empty.
-            // Use Nullable types (with ?) to match type of System.Console.ReadLine
-            string? numInput1 = "";
-            string? numInput2 = "";
-            double result = 0;
-
-            // Ask the user to type the first number.
             Console.Write("Type a number, and then press Enter: ");
-            numInput1 = Console.ReadLine();
+            string? input = Console.ReadLine();
 
-            double cleanNum1 = 0;
-            while (!double.TryParse(numInput1, out cleanNum1))
+            double firstOperand;
+            while (!double.TryParse(input, out firstOperand))
             {
                 Console.Write("This is not valid input. Please enter a numeric value: ");
-                numInput1 = Console.ReadLine();
+                input = Console.ReadLine();
             }
 
-            // Ask the user to type the second number.
             Console.Write("Type another number, and then press Enter: ");
-            numInput2 = Console.ReadLine();
+            input = Console.ReadLine();
 
-            double cleanNum2 = 0;
-            while (!double.TryParse(numInput2, out cleanNum2))
+            double secondOperand;
+            while (!double.TryParse(input, out secondOperand))
             {
                 Console.Write("This is not valid input. Please enter a numeric value: ");
-                numInput2 = Console.ReadLine();
+                input = Console.ReadLine();
             }
 
-            // Ask the user to choose an operator.
             Console.WriteLine("Choose an operator from the following list:");
             Console.WriteLine("\ta - Add");
             Console.WriteLine("\ts - Subtract");
@@ -50,35 +41,45 @@ internal static class Program
             Console.WriteLine("\td - Divide");
             Console.Write("Your option? ");
 
-            string? op = Console.ReadLine();
+            input = Console.ReadLine();
 
-            // Validate input is not null, and matches the pattern
-            if (op == null || !Regex.IsMatch(op, "[a|s|m|d]"))
+            if (input == null || !InputCheck().IsMatch(input))
+            {
                 Console.WriteLine("Error: Unrecognized input.");
+            }
             else
+            {
                 try
                 {
-                    result = calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    double result = calculator.DoOperation(firstOperand, secondOperand, input);
                     if (double.IsNaN(result))
+                    {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
-                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your result: {0:0.##}\n", result);
+                    }
                 }
                 catch (Exception e)
                 {
                     Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
                 }
+            }
 
             Console.WriteLine("------------------------\n");
+            Console.Write("Press 'q' and Enter to close the app, or press Enter to continue: ");
+            if (Console.ReadLine() == "q")
+            {
+                endApp = true;
+            }
 
-            // Wait for the user to respond before closing.
-            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-            if (Console.ReadLine() == "n") endApp = true;
-
-            Console.WriteLine("\n"); // Friendly linespacing.
+            Console.WriteLine("\n");
         }
 
-        // Add call to close the JSON writer before return
         calculator.Finish();
-        return;
     }
+
+    [GeneratedRegex("[a|s|m|d]")]
+    private static partial Regex InputCheck();
 }
